6d85e879d2fd8c31df6dcf1982097bd6f1e02f01
Cleber Moraes <alvesmoraesb@gmail.com>
2025-01-27 22:19:51 -0300

    Carrinho de compras
    Adicionando funcionalidade que inclui produto selecionado no carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let produto = document.getElementById('produto');
+let carrinhoProdutos = document.querySelector('.carrinho__produtos__produto');
+let quantidadeProdutos = document.getElementById('quantidade');
+function adicionar(){ 
+    alert(quantidadeProdutos.value);
+
+    let quantidade = document.createElement('span');
+    quantidade.className = 'texto azul';
+    quantidade.textContent = `${quantidadeProdutos.value}`;
+
+
+    let preco = document.createElement("span");
+    preco.className = "texto-azul";
+    preco.textContent = "R$1400";
+
+
+    let textoProduto = document.createTextNode(" Celular ");
+
+    carrinhoProdutos.appendChild(quantidade);
+    carrinhoProdutos.appendChild(textoProduto);
+    carrinhoProdutos.appendChild(preco);
+    carrinhoProdutos.appendChild(document.createElement("br"));
+
+     
+}
