5490d479b634c9216c88ef357ec3482bb9433956
Cleber Moraes <alvesmoraesb@gmail.com>
2025-02-03 21:32:14 -0300

    Fazendo separação dos valores do input
    usando split para adicionar classes a cada parte do valor do input

--- js/app.js
+++ js/app.js
@@ -1,28 +1,43 @@
-let produtoCarrinho = document.getElementById('produto');
-let carrinhoProdutos = document.querySelector('.carrinho__produtos__produto');
-let quantidadeProdutos = document.getElementById('quantidade');
+
 function adicionar() {
-    alert(produto.value);
 
-    let spanProdutos = document.createElement('span');
-    spanProdutos.className = 'texto-azul';
-    spanProdutos.textContent = `${quantidadeProdutos.value}x`;
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let carrinhoProdutos = document.querySelector('.carrinho__produtos__produto');
+   
+    //alert(nomeProduto);
+    //alert(valorUnitario);
+    //alert(quantidade);
+    //lert(produto);
+    //alert(quantidade);
+    if (quantidade = ""){
+        
+    }
 
+    let spanQuantidadeProdutos = document.createElement('span');
+    spanQuantidadeProdutos.className = 'texto-azul';
+    spanQuantidadeProdutos.textContent = `${quantidade}x `;
 
     let spanPreco = document.createElement("span");
-    spanPreco.className = "texto-azul";
-    spanPreco.textContent = `${produtoCarrinho.value}`;
+    spanPreco.textContent = `${nomeProduto}`;
+
+    let spanValorUnitario = document.createElement("span");
+    spanValorUnitario.className = "texto-azul";
+    spanValorUnitario.textContent = `R$${valorUnitario}`;
 
 
     //let textoProduto = document.createTextNode(" Celular ");
     //let multiplicacao = document.createTextNode(" x ");
 
 
-    carrinhoProdutos.appendChild(spanProdutos);
+    carrinhoProdutos.appendChild(spanQuantidadeProdutos);
     carrinhoProdutos.appendChild(spanPreco);
+    carrinhoProdutos.appendChild(spanValorUnitario);
 
 
-    spanPreco.appendChild(document.createElement("br"));
+    spanValorUnitario.appendChild(document.createElement("br"));
 
 
 }
